3eefc063c97d93b5dd67032d569e183a6b3dcfab
Abishek <87595780+AbishekDk@users.noreply.github.com>
2021-09-01 13:39:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
+<!Doctype html>
+    <body>
+        <div class="center">
+            <form action="index.php" method="POST">
+                <b>Username:</b><input type="text" name="username"><br>
+                <b>Password:</b><input type="password" name="password"><br>
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html>
 <?php
-
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     if ($username =='Abishek' and $password =='aaaaa')
     {
-        include 'test.html';
+        echo '<br/>';
+        echo 'Welcome '.$username;
     }
     else
     {
